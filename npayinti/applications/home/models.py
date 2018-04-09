821f5fc8043b50ee696298d6a56582816c2aeb31
dgafz <csantacruz112@gmail.com>
2018-04-09 18:02:45 -0500

    creacion del proyecto

---
+++ npayinti/applications/home/models.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.template.defaultfilters import slugify
+from django.conf import settings
+# third-party
+from model_utils.models import TimeStampedModel
+from datetime import datetime, timedelta
+
+
+@python_2_unicode_compatible
+class Tag(TimeStampedModel):
+    """ modelo para etiquetas SEO """
+
+    name = models.CharField('nombre', max_length=100)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible
+class Home(TimeStampedModel):
+    """ modelo para la pagina Principal """
+
+    title = models.CharField('titulo', max_length=200, blank=True)
+    description = models.TextField('Descripcion', blank=True)
+    banner_title = models.CharField('Titulo banner', max_length=200)
+    banner_img = models.ImageField('imagen portada', upload_to='home')
+    phone_uno = models.CharField('Telefono 1', max_length=15, blank=True)
+    phone_dos = models.CharField('Telefono 2', max_length=15, blank=True)
+    email = models.EmailField('Email de contacto')
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        verbose_name = 'Pagina Principal'
+        verbose_name_plural = 'Pagina Principal'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class Busquedas(TimeStampedModel):
+    """ modelo para registrar busquedas de clientes """
+
+    name = models.CharField('nombre', max_length=150)
+
+    class Meta:
+        verbose_name = 'Busquedas'
+        verbose_name_plural = 'Busquedas'
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.name
---
+++ requirements/local.py
@@ -0,0 +1,7 @@
+Django==2.0.4
+django-model-utils==3.1.1
+Pillow==5.1.0
+psycopg2-binary==2.7.4
+pytz==2018.3
+sorl-thumbnail==12.4.1
+Unipath==1.1
